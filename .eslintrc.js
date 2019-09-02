c17f52415ce23a88b00bb28f5f7e210f9ad0b764
Santeri Saariokari <santeri.saariokari@tuni.fi>
2019-09-02 13:36:59 +0300

    4.2 blogilista, step2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
     'env': {
-        'browser': true,
         'commonjs': true,
         'es6': true,
         'node': true
@@ -16,11 +15,7 @@ module.exports = {
     'rules': {
         'indent': [
             'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'windows'
+            2
         ],
         'quotes': [
             'error',
@@ -29,6 +24,15 @@ module.exports = {
         'semi': [
             'error',
             'never'
-        ]
+        ],
+        "eqeqeq": "error",
+        "no-trailing-spaces": "error",
+        "object-curly-spacing": [
+            "error", "always"
+        ],
+        "arrow-spacing": [
+            "error", { "before": true, "after": true }
+        ],
+        "no-console": 0
     }
 }
--- index.js
+++ index.js
@@ -1,44 +1,9 @@
-require('dotenv').config()
-
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const server = http.createServer(app)
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-
-app.use(cors())
-app.use(bodyParser.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`)
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
